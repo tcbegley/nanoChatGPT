6bfe9231de8d66afb825fd3ed920f727363d30d2
Alessandro Pietro Bardelli <apbard@users.noreply.github.com>
2023-03-22 12:02:42 +0100

    take USE_GPU knob from env

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
+import os
+from distutils.util import strtobool
 from trainers.trainer import Trainer
 
-USE_GPU = False
+USE_GPU = strtobool(os.environ.get("USE_GPU", "true"))
 
 # Note: the two models are not equivalent. use cpu only for debug
 if USE_GPU:
